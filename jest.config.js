f346010192de64db87b91449aa03f79330c824e3
VigneshUchiha <vigneshponna42@gmail.com>
2026-05-03 20:44:28 +0530

    chore: add jest + rules-unit-testing infrastructure

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'jest-expo',
+  setupFiles: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['<rootDir>/tests/**/*.test.ts', '<rootDir>/tests/**/*.test.tsx'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)/)',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+};
---
+++ jest.setup.ts
@@ -0,0 +1,2 @@
+// Reserved for global test setup. Currently empty; rules tests bring their own.
+export {};
